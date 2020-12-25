a0b1229129094d7470399c80a739a2d332e8cb75
Marina Dariy <dariy.mvs@gmail.com>
2020-12-25 15:10:16 +0300

    new task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,41 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    'use strict';
+        let discriminant = b ** 2 - 4 * a * c;
+        let results = [];
+    if (discriminant >= 0) {
+        let x1 = (- b + Math.sqrt(discriminant)) / (2 * a); 
+        let x2 = (- b - Math.sqrt(discriminant)) / (2 * a);
+        results.push(x1, x2);
+    }
+    else { alert('уравнение не имеет корней');
+    }
+    return results;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    'use strict';
+    if (marks.length === 0) {
+        return 0;
+    }
+    else {
+         if (marks.length > 5) {
+            console.log('Отметок больше 5. Для подсчёта возьмутся первые 5 отметок.');
+         marks.splice(5);
+        }
+        let sum = marks.reduce(add,0);
+        function add(accumulator, a) {
+        return accumulator + a;
+        }
+        let averageMark = sum / marks.length;
+    return averageMark; }
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let today = new Date();
+    let years = today.getFullYear() - dateOfBirthday.getFullYear();
+    if (years >= 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`
+    }
+    else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`}
 }
