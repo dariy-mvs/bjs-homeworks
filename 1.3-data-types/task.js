35682232bb820f1acc0800c3202617f6fa669ae0
Marina Dariy <dariy.mvs@gmail.com>
2020-12-28 11:40:34 +0300

    new task.js/3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,35 +2,32 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let mistake = (variable, variableName) => {
-        alert(`Параметр ${variableName} содержит неправильное значение ${variable}`);
+        return alert(`Параметр ${variableName} содержит неправильное значение ${variable}`);
     };
-    let percentNumber = parseFloat(percent);
+    const percentNumber = parseFloat(percent);
     if (Number.isNaN(percentNumber) === true) {
         mistake(percent, 'процентная ставка');
     };
-    let contributionNumder = parseInt(contribution, 10);
+    const contributionNumder = parseInt(contribution, 10);
     if (Number.isNaN(contributionNumder) === true) {
         mistake(contribution, 'первоначальный взнос');
     };
-    let amountNumber = parseInt(amount, 10);
+    const amountNumber = parseInt(amount, 10);
     if (Number.isNaN(amountNumber) === true) {
         mistake(amount, 'общая стоимость');
     };
-    let debt = amountNumber - contributionNumder;
-    let month = (date.getFullYear() - new Date().getFullYear()) * 12 - new Date().getMonth() + date.getMonth();
-    let p = percentNumber / 100 / 12;
-    let pay = debt * (p + p /(((1 + p)**month)- 1));
-    let totalAmount = parseFloat((pay * month).toFixed(2));
+    const debt = amountNumber - contributionNumder;
+    const month = (date.getFullYear() - new Date().getFullYear()) * 12 - new Date().getMonth() + date.getMonth();
+    const p = percentNumber / 100 / 12;
+    const pay = debt * (p + p /(((1 + p)**month)- 1));
+    const totalAmount = parseFloat((pay * month).toFixed(2));
     return totalAmount;
     
 }
     
 
 
-function getGreeting(name = 'Аноним') {
-    if (name === '') {
-        name = 'Аноним';
-    }
+function getGreeting(name) {
     console.log(`Привет, мир! Меня зовут ${name}.`);
-   return `Привет, мир! Меня зовут ${name}.`;
+   return `Привет, мир! Меня зовут ${name || 'Аноним'}.`;
 }
